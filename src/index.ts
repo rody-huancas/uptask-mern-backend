bd2f19756077c9f0209c53f5a3c3105e57e710f2
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-07 13:00:16 -0500

    Feat: mvc

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(
-    colors.cyan.bold(`Server is running at http://localhost:${port}`)
+    colors.cyan(`Servidor corriendo en la ruta: http://localhost:${port}`)
   );
 });
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import projectRoutes from "./routes/projectRoutes";
 
 // variables de entorno
 dotenv.config();
@@ -9,5 +10,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// Parsear las solicitudes JSON
+app.use(express.json());
+
+// rutas
+app.use("/api/projects", projectRoutes);
 
 export default app;
