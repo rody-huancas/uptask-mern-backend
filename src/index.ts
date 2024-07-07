c370659b8964bef383fc4fe18f5b57a5bf63533c
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-07 11:59:56 -0500

    Feat: add configuration initial

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import colors from "colors";
+import server from "./server";
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(
+    colors.cyan.bold(`Server is running at http://localhost:${port}`)
+  );
+});
