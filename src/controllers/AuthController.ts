6f287cafacef77acace6403ca42f3d6a85999379
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-26 21:58:30 -0500

    Feat: Avoid duplicate users

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,16 +1,23 @@
-import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body;
+      const { password, email } = req.body;
+
+      // prevenir duplicados
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        const error = new Error("El Usuario ya está registrado.");
+        return res.status(409).json({ error: error.message });
+      }
+
       const user = new User(req.body);
 
       // hash password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
+      user.password = await hashPassword(password);
 
       await user.save();
 
