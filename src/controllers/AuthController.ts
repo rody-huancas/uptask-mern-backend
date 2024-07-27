635d1891d088261ceba807589e2fa20a6562a115
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-27 18:48:34 -0500

    Feat: send confirmation email

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,6 +3,7 @@ import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 import Token from "../models/Token";
 import { generateToken } from "../utils/token";
+import { AuthEmail } from "../emails/AuthEmail";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
@@ -27,7 +28,14 @@ export class AuthController {
       token.token = generateToken();
       token.user = user.id;
 
-      await Promise.allSettled([ user.save(), token.save() ]);
+      // enviar email
+      AuthEmail.sendConfirmationEmail({
+        email: user.email,
+        name: user.name,
+        token: token.token,
+      });
+
+      await Promise.allSettled([user.save(), token.save()]);
 
       res.send("Cuenta creada, revisa tu correo para confirmarla.");
     } catch (error) {
---
+++ src/emails/AuthEmail.ts
@@ -0,0 +1,27 @@
+import { transporter } from "../config/nodemailer";
+
+interface IEmail {
+  email: string;
+  name: string;
+  token: string;
+}
+
+export class AuthEmail {
+  static sendConfirmationEmail = async (user: IEmail) => {
+    const info = await transporter.sendMail({
+      from: "UpTask <admin@uptask.com>",
+      to: user.email,
+      subject: "UpTask - Confirma tu cuenta.",
+      text: "UpTask - Confirma tu cuenta.",
+      html: `
+          <p>
+            Hola ${user.name}, has creado tu cuenta en UpTask, ya casi está todo listo, solo debes confirmar tu cuenta
+          </p>
+          <p>Visita el siguiente enlace: </p>
+          <a href="">Confirmar Cuenta</a>
+          <p>E ingresa el código: <b>${user.token}</b></p>
+          <p>Este token expira en 10 minutos</p>
+        `,
+    });
+  };
+}
