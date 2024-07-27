b8326cb0c8e4a007f0cf66b0b6e4f3ce7f978baf
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-26 21:52:38 -0500

    Feat: hash password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,17 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import User from "../models/User";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password } = req.body;
       const user = new User(req.body);
+
+      // hash password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
       await user.save();
 
       res.send("Cuenta creada, revisa tu correo para confirmarla.");
