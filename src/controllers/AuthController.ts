79b3dea03e4416a35c9d55c89f8725cb19e460ad
Rody Huancas <rodyhuancas.04@gmail.com>
2024-07-27 18:26:15 -0500

    Feat: generate token for register user

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
+import Token from "../models/Token";
+import { generateToken } from "../utils/token";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
@@ -19,7 +21,13 @@ export class AuthController {
       // hash password
       user.password = await hashPassword(password);
 
-      await user.save();
+      // generar token
+      const token = new Token();
+
+      token.token = generateToken();
+      token.user = user.id;
+
+      await Promise.allSettled([ user.save(), token.save() ]);
 
       res.send("Cuenta creada, revisa tu correo para confirmarla.");
     } catch (error) {
---
+++ src/models/Token.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IToken extends Document {
+  token: string;
+  user: string;
+  createdAt: string;
+}
+
+const tokenSchema: Schema = new Schema({
+  token: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: String,
+    default: Date.now(),
+    expires: "10min",
+  },
+});
+
+const Token = mongoose.model<IToken>("Token", tokenSchema);
+export default Token;
