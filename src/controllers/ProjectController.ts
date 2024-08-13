7fde63e2d6688a087b590507fd003246d1064f51
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-12 20:46:08 -0500

    Fix: allow the partner to see the project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -20,7 +20,8 @@ export class ProjectController {
     try {
       const projects = await Project.find({
         $or: [
-          { manager: { $in: req.user.id } }
+          { manager: { $in: req.user.id } },
+          { team: {$in: req.user.id} }
         ]
       });
       res.json(projects);
@@ -40,7 +41,7 @@ export class ProjectController {
         return res.status(404).json({ error: error.message });
       }
 
-      if (project.manager.toString() !== req.user.id.toString()) {
+      if (project.manager.toString() !== req.user.id.toString() && !project.team.includes(req.user.id)) {
         const error = new Error('Acción no válida');
         return res.status(404).json({ error: error.message });
       }
