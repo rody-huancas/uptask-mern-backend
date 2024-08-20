dba3f848e7e4f068a2e417fb498183abf62a595c
Rody Huancas <rodyhuancas.04@gmail.com>
2024-08-19 20:05:26 -0500

    Feat: Delete tasks belonging to a project

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,7 +1,8 @@
 import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
 
-import { ITask } from "./Task";
+import Task, { ITask } from "./Task";
 import { IUser } from "./User";
+import Note from "./Note";
 
 export interface IProject extends Document {
   projectName: string;
@@ -20,6 +21,19 @@ const ProjectSchema: Schema = new Schema({
   manager    : { type: Types.ObjectId, ref: 'User' },
   team       : [ { type: Types.ObjectId, ref: 'User' } ],
 }, { timestamps: true } );
+
+// Middleware - eliminar tareas que pertenecen a un proyecto
+ProjectSchema.pre('deleteOne', { document: true }, async function() {
+  const projectId = this._id;
+  if (!projectId) return;
+
+  const tasks = await Task.find({ project: projectId });
+  for (const task of tasks) {
+    await Note.deleteMany({ task: task.id });
+  }
+
+  await Task.deleteMany({ project: projectId }); 
+})
 
 const Project = mongoose.model<IProject>("Project", ProjectSchema);
 export default Project;
